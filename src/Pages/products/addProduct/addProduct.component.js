20a4c398a48836d05c97fd26ae82f918b290428f
vanbalinh123 <vanbalinh080102@gmail.com>
2024-02-21 14:04:42 +0700

    fixed img file

--- src/Pages/products/addProduct/addProduct.component.js
+++ src/Pages/products/addProduct/addProduct.component.js
@@ -30,10 +30,18 @@ import {
   DivImg,
   Box,
   Img,
+  Error,
+  FileInput,
+  CustomButton,
 } from "./addProduct.styles";
 
 const AddProduct = ({ setShowLayout, check, productUpdate }) => {
-  const { register, handleSubmit, setValue } = useForm();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
   const [addProduct] = useAddProductMutation();
   const { data: productGroups } = useGetProductGroupsQuery();
@@ -41,19 +49,28 @@ const AddProduct = ({ setShowLayout, check, productUpdate }) => {
   const [update] = useUpdateProductMutation();
 
   const [file, setFile] = useState({});
+  const [checkImg, setCheckImg] = useState(false);
 
   function handleChange(e) {
     console.log(e.target.files[0]);
     setFile(e.target.files[0]);
     // setValue("img", e.target.files[0]);
+    setCheckImg(true);
   }
 
   useEffect(() => {
     if (check === "update" && productUpdate) {
+      setCheckImg(true);
       setValue("name", productUpdate.name);
       setValue("code", productUpdate.code);
-      setValue("productGroups", productUpdate.productGroup ?  productUpdate.productGroup._id : '');
-      setValue("trademark", productUpdate.trademark ? productUpdate.trademark._id : '');
+      setValue(
+        "productGroups",
+        productUpdate.productGroup ? productUpdate.productGroup._id : ""
+      );
+      setValue(
+        "trademark",
+        productUpdate.trademark ? productUpdate.trademark._id : ""
+      );
       setValue("quantity", productUpdate.quantity);
       setValue("describe", productUpdate.describe);
       setValue("cost", productUpdate.cost);
@@ -85,11 +102,11 @@ const AddProduct = ({ setShowLayout, check, productUpdate }) => {
           "Content-Type": "multipart/form-data",
         });
 
-        if(res.data) {
+        if (res.data) {
           alert("Product added successfully!");
           setShowLayout(false);
         } else {
-          console.log(res)
+          console.log(res);
           alert(res.error.data.message);
         }
       } catch (error) {
@@ -112,22 +129,22 @@ const AddProduct = ({ setShowLayout, check, productUpdate }) => {
           cost: data.cost,
           price: data.price,
           // img: file,
-          img: (String(file) !== '[object Object]') ? file : productUpdate.img
+          img: String(file) !== "[object Object]" ? file : productUpdate.img,
         };
 
-        console.log(productData)
+        console.log(productData);
 
-        console.log(String(file) === '[object Object]')
+        console.log(String(file) === "[object Object]");
 
         const res = await update(productData, {
           "Content-Type": "multipart/form-data",
         });
 
-        if(res.data) {
+        if (res.data) {
           alert("Product updated successfully!");
           setShowLayout(false);
         } else {
-          console.log(res)
+          console.log(res);
           alert(res.error.data.message);
         }
       } catch (error) {
@@ -156,26 +173,27 @@ const AddProduct = ({ setShowLayout, check, productUpdate }) => {
               <SpanNameInput>Mã hàng</SpanNameInput>
               <InputAdd
                 {...register("code", {
-                  required: "Code is required",
+                  required: "Code is required !!!",
                 })}
                 type="text"
               />
-              {/* {errors.code && <div>{errors.code.message}</div>}  */}
+              {errors.code && <Error>{errors.code.message}</Error>}
             </DivInput>
             <DivInput>
               <SpanNameInput>Tên hàng</SpanNameInput>
               <InputAdd
                 {...register("name", {
-                  required: "Name is required",
+                  required: "Name is required !!!",
                 })}
                 type="text"
               />
+              {errors.name && <Error>{errors.name.message}</Error>}
             </DivInput>
             <DivInput>
               <SpanNameInput>Nhóm hàng</SpanNameInput>
               <SelectProductsGroup
                 {...register("productGroups", {
-                  required: "Product Groups is required",
+                  required: "Product Groups is required !!!",
                 })}
               >
                 <Option></Option>
@@ -185,12 +203,15 @@ const AddProduct = ({ setShowLayout, check, productUpdate }) => {
                   </Option>
                 ))}
               </SelectProductsGroup>
+              {errors.productGroups && (
+                <Error>{errors.productGroups.message}</Error>
+              )}
             </DivInput>
             <DivInput>
               <SpanNameInput>Trademark</SpanNameInput>
               <SelectProductsGroup
                 {...register("trademark", {
-                  required: "Trademark is required",
+                  required: "Trademark is required !!!",
                 })}
               >
                 <Option></Option>
@@ -200,16 +221,18 @@ const AddProduct = ({ setShowLayout, check, productUpdate }) => {
                   </Option>
                 ))}
               </SelectProductsGroup>
+              {errors.trademark && <Error>{errors.trademark.message}</Error>}
             </DivInput>
-           
+
             <DivInputRight>
               <SpanNameInputRight>Quantity</SpanNameInputRight>
               <InputAddRight
                 {...register("quantity", {
-                  required: "Quantity is required",
+                  required: "Quantity is required !!!",
                 })}
                 type="number"
               />
+              {errors.quantity && <Error>{errors.quantity.message}</Error>}
             </DivInputRight>
           </LeftLayoutAdd>
           <RightLayoutAdd>
@@ -225,48 +248,83 @@ const AddProduct = ({ setShowLayout, check, productUpdate }) => {
                   onChange={handleChange}
                 />
               </DivInputRight> */}
-               <DivInputRight>
-              <SpanNameInputRight>Cost</SpanNameInputRight>
-              <InputAddRight
-                {...register("cost", {
-                  required: "Cost is required",
-                })}
-                type="number"
-              />
-            </DivInputRight>
-            <DivInputRight>
-              <SpanNameInputRight>Price</SpanNameInputRight>
-              <InputAddRight
-                {...register("price", {
-                  required: "Price is required",
-                })}
-                type="number"
-              />
-            </DivInputRight>
+              <DivInputRight>
+                <SpanNameInputRight>Cost</SpanNameInputRight>
+                <InputAddRight
+                  {...register("cost", {
+                    required: "Cost is required !!!",
+                  })}
+                  type="number"
+                />
+                {errors.cost && <Error>{errors.cost.message}</Error>}
+              </DivInputRight>
+              <DivInputRight>
+                <SpanNameInputRight>Price</SpanNameInputRight>
+                <InputAddRight
+                  {...register("price", {
+                    required: "Price is required !!!",
+                  })}
+                  type="number"
+                />
+                {errors.price && <Error>{errors.price.message}</Error>}
+              </DivInputRight>
               <DivDescribeInput>
                 <SpanNameInput>Desribe</SpanNameInput>
                 <InputDescribeAdd
                   {...register("describe", {
-                    required: "Desribe is required",
+                    required: "Desribe is required !!!",
                   })}
                   type="text"
                 />
+                {errors.describe && <Error>{errors.describe.message}</Error>}
               </DivDescribeInput>
             </FourDivInput>
           </RightLayoutAdd>
           <DivImg>
-            <Box>
-            <Img id="preview-img" src={file.name ? URL.createObjectURL(file) : `http://localhost:3100/img/${productUpdate.img}` }/>
-              {/* <Img src={"http://localhost:3100/img/" + productUpdate.img} /> */}
-            </Box>
-            <input
+            {/* <input
               // {...register("img", {
               //   required: "Image is required",
               // })}
               //   type="text"
               type="file"
               onChange={handleChange}
-            />
+            /> */}
+            {(checkImg && (
+              <CustomButton htmlFor="file-upload">Thay ảnh</CustomButton>
+            )) || (
+              <CustomButton
+                htmlFor="file-upload"
+                {...register("img", {
+                  required: "Image is required !!!",
+                })}
+              >
+                Chọn ảnh
+              </CustomButton>
+            )}
+
+            <FileInput id="file-upload" type="file" onChange={handleChange} />
+            {(checkImg && (
+              <Box>
+                <Img
+                  id="preview-img"
+                  src={
+                    file.name
+                      ? URL.createObjectURL(file)
+                      : `http://localhost:3100/img/${productUpdate.img}`
+                  }
+                />
+                {/* <Img src={"http://localhost:3100/img/" + productUpdate.img} /> */}
+              </Box>
+            )) || (
+              <>
+                {errors.img && (
+                  <div style={{ color: "red", fontStyle: "italic" }}>
+                    {errors.img.message}
+                  </div>
+                )}
+              </>
+            )}
+
             <DivButton>
               {check === "add" ? (
                 <Button type="submit">Add</Button>
--- src/Pages/products/addProduct/addProduct.styles.js
+++ src/Pages/products/addProduct/addProduct.styles.js
@@ -1,199 +1,254 @@
 import { styled } from "styled-components";
 
 export const LayoutAddProduct = styled.div`
-    width: 80%;
-    height: 90%;
-    background-color: #ffffff;
-    z-index: 10;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border-radius: 7px;
-    backdrop-filter: blur(20px);
-    box-shadow: 0px 0px 20px 1px grey;
-    color: #696969;
+  width: 80%;
+  height: 90%;
+  background-color: #ffffff;
+  z-index: 10;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 7px;
+  backdrop-filter: blur(20px);
+  box-shadow: 0px 0px 20px 1px grey;
+  color: #696969;
 `;
 
 export const FormAdd = styled.form`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const TitleLayoutAdd = styled.span`
-    padding: 38px;
-    font-size:20px;
-    font-weight: bold;
+  padding: 38px;
+  font-size: 20px;
+  font-weight: bold;
 `;
 
 export const MainLayoutAdd = styled.div`
-    display: flex;
-    width: 90%;
-    margin: 0 auto;
-    height: 100%;
-    margin-bottom: 40px;
-    gap: 5%;
+  display: flex;
+  width: 90%;
+  margin: 0 auto;
+  height: 100%;
+  margin-bottom: 40px;
+  gap: 5%;
 `;
 
 //left layout
 export const LeftLayoutAdd = styled.div`
-    width: 50%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
 `;
 
 export const DivInput = styled.div`
-    width: 100%;
-    /* height: 100%; */
-    /* flex: 1; */
-    display: flex;
-    align-items: center;
-    height: 100px;
+  width: 100%;
+  /* height: 100%; */
+  /* flex: 1; */
+  display: flex;
+  align-items: center;
+  height: 100px;
+  position: relative;
 `;
 
 export const SpanNameInput = styled.span`
-    flex: 1;
+  flex: 1;
+
+  &::after {
+    content: "*";
+    color: red;
+    padding-left: 10px;
+  }
 `;
 
 export const InputAdd = styled.input`
-flex: 3;
-    height: 40px;
-    width: 100%;
-    border: none;
-    border-radius: 7px;
-    background-color: #F5F5F5;
-    box-shadow: grey 0px 1px 2px;
-    transition: all 0.3s;
-    color: #696969;
+  flex: 3;
+  height: 40px;
+  width: 100%;
+  border: none;
+  border-radius: 7px;
+  background-color: #f5f5f5;
+  box-shadow: grey 0px 1px 2px;
+  transition: all 0.3s;
+  color: #696969;
 `;
 
 export const SelectProductsGroup = styled.select`
-flex: 3;
-    height: 40px;
-    width: 100%;
-    border: none;
-    border-radius: 7px;
-    background-color: #F5F5F5;
-    box-shadow: grey 0px 1px 2px;
-    transition: all 0.3s;
-    color: #696969;
+  flex: 3;
+  height: 40px;
+  width: 100%;
+  border: none;
+  border-radius: 7px;
+  background-color: #f5f5f5;
+  box-shadow: grey 0px 1px 2px;
+  transition: all 0.3s;
+  color: #696969;
 `;
 
-export const Option = styled.option`
-    
-`;
+export const Option = styled.option``;
 
 export const DivDescribeInput = styled.div`
-    width: 100%;
-    display: flex;
-    /* flex-direction: column; */
-    padding-top: 40px;
-    gap: 10px;
+  width: 100%;
+  display: flex;
+  /* flex-direction: column; */
+  padding-top: 40px;
+  gap: 10px;
+  position: relative;
+  padding-bottom: 30px;
 `;
 
 export const InputDescribeAdd = styled.textarea`
-flex: 3;
-    min-height: 260px;
-    border: none;
-    border-radius: 7px;
-    background-color: #F5F5F5;
-    box-shadow: grey 0px 1px 2px;
-    transition: all 0.3s;
-    color: #696969;    
+  flex: 3;
+  min-height: 260px;
+  border: none;
+  border-radius: 7px;
+  background-color: #f5f5f5;
+  box-shadow: grey 0px 1px 2px;
+  transition: all 0.3s;
+  color: #696969;
 `;
 
 //right layout
 export const RightLayoutAdd = styled.div`
-    width: 50%;
-    display: flex;
-    flex-direction: column;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const FourDivInput = styled.div`
-    flex: 2;
-    display: flex; 
-    flex-direction: column;
-    gap: 30px;
-`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
 
 export const DivInputRight = styled.div`
-    /* flex: 1; */
-    display: flex;
-    align-items: center;
-    height: 100px;
-`
+  /* flex: 1; */
+  display: flex;
+  align-items: center;
+  height: 100px;
+  position: relative;
+`;
 
 export const SpanNameInputRight = styled.span`
-    flex: 1;
+  flex: 1;
+
+  &::after {
+    content: "*";
+    color: red;
+    padding-left: 10px;
+  }
 `;
 
 export const InputAddRight = styled.input`
-flex: 3;
-width: 100%;
-    height: 40px;
-    border: none;
-    border-radius: 7px;
-    background-color: #F5F5F5;
-    box-shadow: grey 0px 1px 2px;
-    transition: all 0.3s;
-    color: #696969; 
+  flex: 3;
+  width: 100%;
+  height: 40px;
+  border: none;
+  border-radius: 7px;
+  background-color: #f5f5f5;
+  box-shadow: grey 0px 1px 2px;
+  transition: all 0.3s;
+  color: #696969;
 `;
 
 export const DivButton = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 20px;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
 
 export const Button = styled.button`
-    flex:1 ;
-    height: 50px;
-    border: none;
-    border-radius: 7px;
-    background-color: #F5F5F5;
-    box-shadow: grey 0px 2px 6px;
-    transition: all 0.3s;
-    color: #696969;
+  flex: 1;
+  height: 50px;
+  border: none;
+  border-radius: 7px;
+  background-color: #f5f5f5;
+  box-shadow: grey 0px 2px 6px;
+  transition: all 0.3s;
+  color: #696969;
 
-    &:hover{
-        cursor: pointer;
-        opacity: 0.8;
-        box-shadow: grey 0px 1px 1px;
-    }
-`
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+    box-shadow: grey 0px 1px 1px;
+  }
+`;
 
 //img
 
 export const DivImg = styled.div`
-    width: 600px;
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
-    position: relative;
-`
+  width: 600px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  position: relative;
+`;
 
 export const Box = styled.div`
-    width: 400px;
-    height: 400px;
-    border-radius: 10px;
-    box-shadow: grey 0px 2px 6px;
-
-`
-
+  width: 400px;
+  height: 460px;
+  border-radius: 10px;
+  box-shadow: grey 0px 2px 6px;
+`;
 
 export const Img = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  /* box-shadow: grey 0px 2px 6px; */
+
+`;
+
+export const Error = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  color: red;
+  font-style: italic;
+`;
+
+export const FileInput = styled.input`
+  display: none;
+`;
+
+export const CustomButton = styled.label`
+  /* padding: 10px 20px; */
+  height: 40px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  margin-top: 30px;
+  cursor: pointer;
+  border: none;
+  border-radius: 7px;
+  background-color: #f5f5f5;
+  box-shadow: grey 0px 2px 6px;
+  transition: all 0.3s;
+  color: #696969;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+    box-shadow: grey 0px 1px 1px;
+  }
 
 
+  &::after {
+    content: "*";
+    color: red;
+    padding-left: 10px;
+  }
+`;
